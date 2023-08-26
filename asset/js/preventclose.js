1a72f5d0f9aa6914c10cd5ff9149c1b007b2974f
hwtam <hwtamad@connect.ust.hk>
2023-08-26 15:58:21 +0800

    add new favicon

--- asset/js/preventclose.js
+++ asset/js/preventclose.js
@@ -6,8 +6,29 @@ window.onbeforeunload = function() {
 // others
 window.onpageshow = function() {
   document.getElementById("changable").style = "display: inline-block";
+  changeFavicon("/asset/img/warning.ico");
 };
 
 window.onclick = function() {
   document.getElementById("changable").style = 'display: none';
+  changeFavicon("/asset/img/brain.ico");
 };
+
+// https://poe.com/s/loMCUJOXr7QJvDFmKarp
+function changeFavicon(newFaviconUrl) {
+  const head = document.head;
+  const existingFavicon = document.querySelector('link[rel="icon"]');
+
+  // Create a new link element for the favicon
+  const newFavicon = document.createElement('link');
+  newFavicon.rel = 'icon';
+  newFavicon.href = newFaviconUrl;
+
+  // Replace the existing favicon if it exists
+  if (existingFavicon) {
+    head.removeChild(existingFavicon);
+  }
+
+  // Append the new favicon to the head
+  head.appendChild(newFavicon);
+}
