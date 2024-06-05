ab0ef5c9e93d4e913adc8ec0bcbd4b0006167738
tomtom <hwtamad@connect.ust.hk>
2024-06-05 17:54:06 +0800

    try totally disable tab close

--- asset/js/preventclose.js
+++ asset/js/preventclose.js
@@ -1,7 +1,5 @@
 // main
-window.onbeforeunload = function() {
-  return "Would you really like to close your browser?";
-};
+window.onbeforeunload = preventDefault;
 
 // others
 window.onpageshow = function() {
